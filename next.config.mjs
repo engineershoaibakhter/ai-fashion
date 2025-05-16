2c45514300418640e78b3c0c2b4a7342fc7b6514
Shoaib Akhter <shoaibakhter181422@gmail.com>
2025-05-16 16:55:13 +0500

    feat: add global styles and layout for Next.js app
    - Introduced global CSS variables for light and dark themes in globals.css
    - Created a RootLayout component in layout.js to wrap the application with font styles
    - Developed the main page structure in page.js with responsive design and CTAs
    - Added styles for the main page in page.module.css, including button styles and layout adjustments
    - Configured ESLint with Next.js core web vitals in eslint.config.mjs
    - Included SVG assets for icons in the public directory (file.svg, globe.svg, window.svg)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['s3-us-west-2.amazonaws.com'], // Add the hostname(s) of your image sources
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
-
